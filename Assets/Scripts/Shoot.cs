e6afb527e374e07a32137dfaf921f54e3b7ceaac
Relaned <Relaned@yandex.ru>
2020-05-10 17:36:49 +0300

    Сделал стрельбу и новый спрайт для анититела
    А планировал кручение эномисов

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject ShootingAntibody;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shooting();
+        }
+        
+    }
+
+    void Shooting()
+    {
+        Instantiate(ShootingAntibody, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Assets/Scripts/ShootingAntybody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShootingAntybody : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+}
